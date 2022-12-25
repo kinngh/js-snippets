69aa5449ce5a928cdf7e353215422441c323708e
Kinngh <kinngh@me.com>
2022-12-25 13:23:34 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# JS Snippets
+
+A collection of snippets from across the internet to do tasks.
+
+## Index
+
+- [Wait for element to exist](./code//element-exist.js): Wait for HTML element to exist and then run JS on it.
---
+++ code/element-exist.js
@@ -0,0 +1,25 @@
+function waitForElementToExist(selector) {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver((mutations) => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
+
+// Usage
+
+waitForElementToExist(".className").then((ele) => { //Class name needs to start with a `.`
+  alert("Element now exists!", ele);
+});
